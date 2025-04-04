7fcdee86d5ba4629ef93b965243ff753decc60fe
LRLayan <rameshlayan4@gmail.com>
2025-04-04 09:47:15 +0530

    remove unused import

--- src/main/java/com/example/interviewsentura/controller/UserController.java
+++ src/main/java/com/example/interviewsentura/controller/UserController.java
@@ -4,13 +4,9 @@ import com.example.interviewsentura.dto.UserDTO;
 import com.example.interviewsentura.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -25,8 +21,7 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable String userId,
-                                              @Valid @RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String userId, @Valid @RequestBody UserDTO userDTO) {
         UserDTO updatedUser = userService.updateUser(userId, userDTO);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
