e47a44209d771bcc6fe02609bcd2388c870e8825
LRLayan <rameshlayan4@gmail.com>
2025-04-04 09:31:26 +0530

    saveUser

--- src/main/java/com/example/interviewsentura/controller/UserController.java
+++ src/main/java/com/example/interviewsentura/controller/UserController.java
@@ -1,7 +1,23 @@
 package com.example.interviewsentura.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.interviewsentura.dto.UserDTO;
+import com.example.interviewsentura.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/user")
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> saveUser(@Valid @RequestBody UserDTO userDTO) {
+        UserDTO saveUser = userService.saveUser(userDTO);
+        return new ResponseEntity<>(saveUser, HttpStatus.CREATED);
+    }
 }
